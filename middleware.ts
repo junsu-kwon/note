2df1862578a28c12f35052f05a14ebe030803da8
junsu-kwon <kjs940322@gmail.com>
2023-04-10 10:20:47 +0900

    230410 공부

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,14 @@
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
+// 기본적으로 전체 페이지에서 실행
 export function middleware(request: NextRequest) {
-  // ...
+  // redirect사용 예시
+  if (request.nextUrl.pathname.startsWith('/products/1004')) {
+    return NextResponse.redirect(new URL('/products', request.url));
+  }
 }
+
+// matcher 옵션을 이용하여 특정 경로에서만 middleware가 동작하도록 설정
+export const config = {
+  matcher: ['/products/:path*'], // products 경로 밑으로 전부 실행
+};
